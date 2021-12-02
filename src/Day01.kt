7c61c1ee09860ed59d1652755d55c97a03c454d7
Akhil Nambiar <UB15WY@ing.com>
2021-12-02 07:55:58 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        return (0..input.size - 2)
+            .count {
+                input[it + 1].toInt() > input[it].toInt()
+            }
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intList = input.map { it.toInt() }
+        return (0..input.size - 4).count {
+            print(intList.subList(it, it + 3))
+            println( intList.subList(it + 1, it + 4))
+            intList.subList(it, it + 3).sum() < intList.subList(it + 1, it + 4).sum()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
+//    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    check(part1(testInput) == 1)
 
+    check(part2(testInput) == 5)
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
